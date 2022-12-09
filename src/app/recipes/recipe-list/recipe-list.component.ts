311eb67e54874484bf75e902a047a756e3f5d139
vivek.shukla@cgi.com <vivek.shukla@cgi.com>
2022-12-09 15:04:08 -0600

    Chapter 3 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.istockphoto.com/id/1197494143/photo/men-eating-vegan-creamy-roasted-pumpkin-soup.jpg?s=612x612&w=is&k=20&c=jwB_JzudlI5sz_P9-mFDi1Z-NxvlPX9GXJLtsOBdTQw=')
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    //new way of initializing properties/variables...
+    constructor(public name: string,public amount:number){} 
+}
